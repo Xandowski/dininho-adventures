5fe048831be50a17fa6269623c2b7d3bf356ecac
Xandowski <xandy-moraes@hotmail.com>
2020-04-03 13:11:34 -0300

    finalizado carrosel

--- js/components/carousel-dino.js
+++ js/components/carousel-dino.js
@@ -23,9 +23,32 @@
     $left.classList.add('-left');
   }
 
+  function rightClick() {
+    let $left = $carouselDino.querySelector('.-left');
+    let $right = $carouselDino.querySelector('.-right');
+    let $center = $carouselDino.querySelector('.-center');
+
+    const id = parseInt($left.getAttribute('data-id'), 10);
+    const nextId = id === 0 ? 15 : id - 1;
+
+    $right.classList.remove('-right');
+    $center.classList.remove('-center');
+    $left.classList.remove('-left');
+
+    $right.classList.add('-center');
+    $center.classList.add('-left');
+
+    $left = $center;
+    $center = $right;
+    $right = $carouselDino.querySelector(`[data-id="${nextId}"]`);
+    $right.classList.add('-right');
+  }
+
   $carouselDino.addEventListener('click', ({ target }) => {
     if (target.parentNode.classList.contains('-left')) {
       leftClick();
+    } else if (target.parentNode.classList.contains('-right')) {
+      rightClick();
     }
   });
 })();
--- js/video-dino.js
+++ js/video-dino.js
@@ -2,10 +2,12 @@ const $wrapperVideoDino = document.querySelector('.wrapper-video-dino');
 const $videoDino = document.querySelector('.video-dino');
 
 $wrapperVideoDino.addEventListener('click', () => {
-  if ($videoDino.paused) {
-    $videoDino.play();
-  } else {
-    $videoDino.pause();
+  if ($wrapperVideoDino.parentNode.classList.contains('-center')) {
+    if ($videoDino.paused) {
+      $videoDino.play();
+    } else {
+      $videoDino.pause();
+    }
+    $wrapperVideoDino.classList.toggle('-play');
   }
-  $wrapperVideoDino.classList.toggle('-play');
 });
